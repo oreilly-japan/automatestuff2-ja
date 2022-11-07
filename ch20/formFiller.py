44e59ab3a9bfc1297e2520144b4ef42ff270b133
aizoaikawa <aizoaikawa@gmail.com>
2022-11-07 10:06:26 +0900

    fix formFiller.py for macOS

--- ch20/formFiller.py
+++ ch20/formFiller.py
@@ -1,19 +1,28 @@
 #! python3
 # formFiller.py - フォームの自動入力
 
-import pyautogui, time
+import pyautogui, time, pyperclip
 
-form_data = [{'name': 'Alice', 'fear': 'eavesdroppers', 'source': 'wand',
-              'robocop': 4, 'comments': 'Tell Bob I said hi.'},
-             {'name': 'Bob', 'fear': 'bees', 'source': 'amulet', 'robocop': 4,
-              'comments': 'n/a'},
-             {'name': 'Carol', 'fear': 'puppets', 'source': 'crystal ball',
-              'robocop': 1,
-              'comments': 'Please take the puppets out of the break room.'},
-             {'name': 'Alex Murphy', 'fear': 'ED-209', 'source': 'money',
-             'robocop': 5,
-              'comments': 'Protect the innocent. Serve the public trust. Uphold the law.'},
-             ]
+# pyperclip.write()の代わり
+def mywrite(s):
+    saved_clipboard = pyperclip.paste()
+    pyperclip.copy(s)
+    pyautogui.hotkey('ctrl', 'v') # macOSの場合は 'command'
+    pyperclip.copy(saved_clipboard)
+
+form_data = [
+  {'name': 'Alice', 'fear': 'eavesdroppers', 'source': 'wand',
+   'robocop': 4, 'comments': 'Tell Bob I said hi.'},
+  {'name': 'Bob', 'fear': 'bees', 'source': 'amulet',
+   'robocop': 4,  'comments': 'n/a'},
+  {'name': 'Carol', 'fear': 'puppets', 'source': 'crystal ball',
+   'robocop': 1,
+   'comments': 'Please take the puppets out of the break room.'},
+  {'name': 'Alex Murphy', 'fear': 'ED-209', 'source': 'money',
+   'robocop': 5,
+   'comments':
+     'Protect the innocent. Serve the public trust. Uphold the law.'},
+]
 
 pyautogui.PAUSE = 0.5
 print('ブラウザウィンドウがアクティブでありフォームが読み込まれていることを確認してください！')
@@ -24,38 +33,28 @@ for person in form_data:
     time.sleep(5) # ❶
 
     print(f'{person["name"]}の情報を入力中...')  # ❶
-    pyautogui.write(['\t', '\t'])  # ❷
+    pyautogui.write('\t' * 4)  # ❷
 
     # Name欄を入力する
-    pyautogui.write(person['name'] + '\t')  # ❸
+    mywrite(person['name'])  # ❸
+    pyautogui.write('\t')  # ❸
 
     # Greatest Fear(s)欄を入力する
-    pyautogui.write(person['fear'] + '\t')  # ❹
+    mywrite(person['fear'])  # ❹
+    pyautogui.write('\t')  # ❹
 
     # Source of Wizard Powers欄を選択する
-    if person['source'] == 'wand':  # ❶
-        pyautogui.write(['down', 'enter', '\t'], 0.5)  # ❷
-    elif person['source'] == 'amulet':
-        pyautogui.write(['down', 'down', 'enter', '\t'], 0.5)
-    elif person['source'] == 'crystal ball':
-        pyautogui.write(['down', 'down', 'down', 'enter', '\t'], 0.5)
-    elif person['source'] == 'money':
-        pyautogui.write(['down', 'down', 'down', 'down', 'enter', '\t'], 0.5)
+    n = ['wand', 'amulet',
+         'crystal ball', 'money'].index(person['source']) # ❶
+    pyautogui.write(['down'] * (n + 1) + ['enter', '\t'], 0.5)  # ❷
 
     # RoboCop欄を選択する
-    if person['robocop'] == 1:  # ❸
-        pyautogui.write([' ', '\t', '\t'], 0.5)  # ❹
-    elif person['robocop'] == 2:
-        pyautogui.write(['right', '\t', '\t'], 0.5)
-    elif person['robocop'] == 3:
-        pyautogui.write(['right', 'right', '\t', '\t'], 0.5)
-    elif person['robocop'] == 4:
-        pyautogui.write(['right', 'right', 'right', '\t', '\t'], 0.5)
-    elif person['robocop'] == 5:
-        pyautogui.write(['right', 'right', 'right', 'right', '\t', '\t'], 0.5)
+    robocop = person['robocop'] # ❸
+    pyautogui.write([' '] + ['right'] * (robocop-1) + '\t'*2, 0.5)  # ❹
 
     # Additional Comments欄を入力する
-    pyautogui.write(person['comments'] + '\t')
+    mywrite(person['comments'])
+    pyautogui.write('\t')
 
     # Submitをクリックする
     time.sleep(0.5) # ボタンが有効になるのを待つ
